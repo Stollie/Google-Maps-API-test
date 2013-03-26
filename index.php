d06ac796c0610eba9a90c0521ea02e5da226affd
Stollie <remco.raaymakers@gmail.com>
2013-03-26 12:43:36 +0100

    submit opvangen

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link href="css/site.css" rel="stylesheet" />
   </head>
   <body>
-    <div class="container">
+    <div id="container">
         <div class="row">
             <div class="span9" id="map-canvas"></div>
         </div>
@@ -23,17 +23,11 @@
     <script type="text/javascript"
       src="https://maps.googleapis.com/maps/api/js?sensor=false&libaries=geometry">
     </script>
-    <script type="text/javascript">
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
-    </script>    
+    <script type="text/javascript"
+      src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.9.1.min.js">
+    </script>
+    <script type="text/javascript"
+      src="js/site.js">
+    </script>   
   </body>
 </html>
